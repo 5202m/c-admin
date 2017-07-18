bb092230207576fb2e32b7ca2309a17d6cce41e7
Eugene Zeng <euene.zeng@live.com>
2017-07-18 16:47:14 +0800

    文件压缩的代码修改。

--- util/fileZip.js
+++ util/fileZip.js
@@ -77,27 +77,7 @@ var fileZip = {
 
 //直播间前端js/css压缩
 var fileList = [
-    // FX-theme1
-    { "out": "\\template\\theme1\\static\\css\\index.min.css", "in": ["\\template\\theme1\\static\\css\\index.css"] },
-    { "out": "\\template\\theme1\\static\\css\\train.min.css", "in": ["\\template\\theme1\\static\\css\\train.css"] },
-    { "out": "\\template\\theme1\\static\\js\\lib.min.js", "marge": true, "in": ["\\template\\base\\lib\\jquery-1.7.min.js", "\\template\\base\\lib\\socket.io.js", "\\template\\base\\lib\\mScroll\\jquery.mCustomScrollbar.concat.min.js", "\\template\\base\\lib\\swipe\\swiper.min.js", "\\template\\base\\lib\\lightbox\\lightbox.min.js", "\\template\\base\\lib\\jqueryUI\\jquery-ui.min.js", "\\template\\base\\lib\\jquery.xdomainrequest.min.js", "\\template\\base\\lib\\jquery.superslide.2.1.1.js", "\\template\\base\\lib\\websocket.min.js", "\\template\\base\\lib\\newquote.min.js", "\\template\\base\\lib\\obsplayer\\obsplayer.min.js", "\\template\\base\\lib\\sewise\\sewise.player.min.js", "\\template\\base\\lib\\paste.min.js", "\\template\\base\\lib\\jquery.face.min.js"] },
-    { "out": "\\template\\theme1\\static\\js\\index.min.js", "marge": false, "in": ["\\template\\base\\util\\common.js", "\\template\\base\\util\\gwanalysis.js", "\\template\\base\\util\\chatAnalyze.js", "\\template\\theme1\\static\\js\\index.js", "\\template\\theme1\\static\\js\\index_tool.js", "\\template\\theme1\\static\\js\\index_box.js", "\\template\\theme1\\static\\js\\index_video.js", "\\template\\theme1\\static\\js\\index_video_live.js", "\\template\\theme1\\static\\js\\index_video_teach.js", "\\template\\theme1\\static\\js\\index_video_subscribe.js", "\\template\\theme1\\static\\js\\index_video_train.js", "\\template\\theme1\\static\\js\\index_chat_pride.js", "\\template\\theme1\\static\\js\\index_chat.js", "\\template\\theme1\\static\\js\\index_chat_teacher.js", "\\template\\theme1\\static\\js\\index_chat_showtrade.js", "\\template\\theme1\\static\\js\\index_notice.js"] },
-    { "out": "\\template\\theme1\\static\\js\\lg.min.js", "marge": false, "in": ["\\template\\theme1\\static\\js\\loginAuto.js"] },
-
-    // FX-theme2
-    { "out": "\\template\\theme2\\static\\css\\index.min.css", "in": ["\\template\\theme2\\static\\css\\index.css"] },
-    { "out": "\\template\\theme2\\static\\js\\index.min.js", "marge": false, "in": ["\\template\\base\\util\\gwanalysis.js", "\\template\\base\\util\\chatAnalyze.js", "\\template\\base\\util\\common.js", "template\\theme2\\static\\js\\tool.js", "\\template\\theme2\\static\\js\\index.js"] },
-    { "out": "\\template\\theme2\\static\\js\\room.min.js", "marge": false, "in": ["\\template\\base\\util\\gwanalysis.js", "\\template\\base\\util\\chatAnalyze.js", "\\template\\base\\util\\common.js", "template\\theme2\\static\\js\\tool.js", "\\template\\theme2\\static\\js\\room.js"] },
-    { "out": "\\template\\theme2\\static\\js\\pop.min.js", "marge": false, "in": ["\\template\\theme2\\static\\js\\pop.js"] },
-    { "out": "\\template\\theme2\\static\\js\\lg.min.js", "marge": false, "in": ["\\template\\theme2\\static\\js\\loginAuto.js"] },
-
-    // FX-theme4
-    { "out": "\\template\\theme4\\static\\css\\index.min.css", "in": ["\\template\\theme4\\static\\css\\index.css"] },
-    { "out": "\\template\\theme4\\static\\css\\index-dark.min.css", "in": ["\\template\\theme4\\static\\css\\index-dark.css"] },
-    { "out": "\\template\\theme4\\static\\js\\room.min.js", "marge": false, "in": ["\\template\\base\\util\\gwanalysis.js", "\\template\\base\\util\\chatAnalyze.js", "\\template\\base\\util\\common.js", "\\template\\theme4\\static\\js\\room.js", "\\template\\base\\lib\\obsplayer\\obsplayer.min.js"] },
-    { "out": "\\template\\theme4\\static\\js\\pop.min.js", "marge": false, "in": ["\\template\\theme4\\static\\js\\pop.js"] },
-    { "out": "\\template\\theme4\\static\\js\\lg.min.js", "marge": false, "in": ["\\template\\theme4\\static\\js\\loginAuto.js"] },
-
+    
     // ADMIN
     { "out": "\\template\\admin\\static\\css\\index.min.css", "in": ["\\template\\admin\\static\\css\\index.css"] },
     { "out": "\\template\\admin\\static\\css\\room.min.css", "in": ["\\template\\admin\\static\\css\\room.css"] },
